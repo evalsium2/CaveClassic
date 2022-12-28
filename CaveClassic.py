98170403cf60ccdc61cfaaa9d6c3eff0ae5d898e
evalsium2 <117542711+evalsium2@users.noreply.github.com>
2022-12-28 20:34:02 +0300

    Add files via upload

---
+++ CaveClassic.py
@@ -0,0 +1,42 @@
+from ursina import *
+from ursina.prefabs. \
+    first_person_controller \
+    import FirstPersonController
+import pygame
+
+app = Ursina()
+Sky(texture="sky/sky1")
+player = FirstPersonController()
+
+boxes = []
+for n in range(50):
+    for k in range(50):
+        box = Button(color=color.white,
+                     model="cube",
+                     position=(k, 0, n),
+                     texture='assets/grass',
+                     parent=scene,
+                     origin_y=0.5
+                     )
+
+        boxes.append(box)
+
+
+def input(key):
+    for box in boxes:
+        if box.hovered:
+            if key == "left mouse down":
+                new = Button(color=color.white,
+                             model="cube",
+                             position=
+                             box.position + mouse.normal,
+                             texture='assets/stone',
+                             parent=scene,
+                             origin_y=0.5)
+                boxes.append(new)
+            if key == "right mouse down":
+                boxes.remove(box)
+                destroy(box)
+
+
+app.run()
